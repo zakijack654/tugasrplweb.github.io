5e21d1bd91bad7caaadf0568a48e4ae8cc285730
MuhammadNZaki <zakijack654@gmail.com>
2021-12-29 15:36:31 +0700

    Add files via upload

---
+++ link/notification/data/games.js
@@ -0,0 +1,26 @@
+var games = [
+  {
+    slug: 'Ini notifikasi',
+    name: 'Halo',
+    author: 'Kelompok 9',
+    twitter: 'Cek notif',
+    website: '',
+   
+  },
+  {
+    slug: 'Ini notifikasi',
+    name: 'Halo',
+    author: 'Kelompok 9',
+    twitter: 'Cek notif',
+    website: '',
+   
+  },
+  {
+    slug: 'Ini notifikasi',
+    name: 'Halo',
+    author: 'Kelompok 9',
+    twitter: 'Cek notif',
+    website: '',
+   
+  },
+];
